bb1f9c315772dcb5dfd55dc9d8171c213ab40f53
greenvilleassociates <55986791+greenvilleassociates@users.noreply.github.com>
2019-11-14 18:33:16 -0500

    Update windows1500install.bat

--- templates/windows1500install.bat
+++ templates/windows1500install.bat
@@ -1,2 +1,4 @@
-rem copy 1500pxframe.index.html c:\web\target
-rem copy framebase.html c:\web\target
+rem On an IIS Web Server default directory will be HTML Root of c:\inetpub\wwwroot
+rem remove the comments and execute from command line in the templates directory (IE where you git cloned the source)
+rem copy 1500pxframe.index.html c:\inetpub\wwwroot\fusionshelltarget\
+rem copy framebase.html c:\inetpub\wwwroot\fusionshelltarget\
